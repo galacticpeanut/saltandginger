5d0538dbc651e9578111977e7ad39bd102ed7530
Robert Ziegler <zieglerrp@protonmail.com>
2020-03-16 19:37:03 -0700

    Update ports

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -12,13 +12,13 @@ export class MenuService {
   constructor(private httpService: HttpClient) { }
 
   public getItem(id: number): Observable<Menu> {
-    return this.httpService.get<Menu>(`http://api.saltandginger.betazeta.io:8081/api/menu/${id}`).pipe(
+    return this.httpService.get<Menu>(`http://api.saltandginger.betazeta.io/api/menu/${id}`).pipe(
       map(data => new Menu().deserialize(data))
     );
   }
 
   public getAllItems(): Observable<Menu[]> {
-    return this.httpService.get<Menu[]>(`http://api.saltandginger.betazeta.io:8081/api/menu`).pipe(
+    return this.httpService.get<Menu[]>(`http://api.saltandginger.betazeta.io/api/menu`).pipe(
       map(data => data['menu'].map(data => new Menu().deserialize(data)))
     );
   }
